66e935493292845c57fddd0f532cce682801b386
wandalen <wandalen.me@gmail.com>
2017-05-14 22:28:09 +0400

    publish

--- staging/amid/z.test/Deployer.test.s
+++ staging/amid/z.test/Deployer.test.s
@@ -17,11 +17,14 @@ if( typeof module !== 'undefined' )
 
   require( 'wTools' );
 
-
-  if( require( 'fs' ).existsSync( __dirname + '/../../amid/diagnostic/Testing.debug.s' ) )
-  require( '../../amid/diagnostic/Testing.debug.s' );
-  else
-  require( 'wTesting' );
+  try
+  {
+    require( '../../amid/diagnostic/Testing.debug.s' );
+  }
+  catch( err )
+  {
+    require( 'wTesting' );
+  }
 
 }
 
